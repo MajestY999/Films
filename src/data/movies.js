f210c25e306caeba4962054c7f13b2abf5850b99
MajestY999 <Vilisovartem@gmail.com>
2025-06-08 23:39:10 +1000

    first commit

---
+++ src/data/movies.js
@@ -0,0 +1,99 @@
+import punisher from '@/assets/images/Punisher2017.png'
+import rocky4 from '@/assets/images/Rocky4.png'
+import matrix from '@/assets/images/Matrix.png'
+import interstellar from '@/assets/images/Interstellar.png'
+import madMax from '@/assets/images/MadMaxFuryRoad.png'
+import bastards from '@/assets/images/InglouriousBasterds.png'
+import hatefulEight from '@/assets/images/HatefulEight.png'
+import joker from '@/assets/images/Joker2019.png'
+
+export default [
+  {
+    id: 1,
+    title: "Каратель",
+    genre: "боевик, криминал",
+    image: punisher,
+    imagePath: 'assets/images/Punisher2017.png',
+    shortDesc: "Бывший морпех начинает войну против преступников после убийства семьи.",
+    year: 2017,
+    director: "Стив Лайтфут",
+    fullDesc: "Фрэнк Касл становится Карателем, беспощадным мстителем, раскрывающим коррупцию и тайные операции."
+  },
+  {
+    id: 2,
+    title: "Рокки 4",
+    genre: "драма, спорт",
+    image: rocky4,
+    imagePath: 'assets/images/Rocky4.png',
+    shortDesc: "Рокки Бальбоа выходит на бой против Ивана Драго в сердце СССР.",
+    year: 1985,
+    director: "Сильвестр Сталлоне",
+    fullDesc: "В патриотичном и эмоциональном поединке Рокки борется за честь друга и нации."
+  },
+  {
+    id: 3,
+    title: "Матрица",
+    genre: "фантастика, боевик",
+    image: matrix,
+    imagePath: 'assets/images/Matrix.png',
+    shortDesc: "Компьютерный хакер Нео узнаёт о настоящей природе реальности.",
+    year: 1999,
+    director: "Лана и Лилли Вачовски",
+    fullDesc: "Нео вступает в борьбу против машин, контролирующих виртуальный мир под названием Матрица."
+  },
+  {
+    id: 4,
+    title: "Интерстеллар",
+    genre: "фантастика, драма",
+    image: interstellar,
+    imagePath: 'assets/images/Interstellar.png',
+    shortDesc: "Астронавты ищут новый дом для человечества за пределами Солнечной системы.",
+    year: 2014,
+    director: "Кристофер Нолан",
+    fullDesc: "Эпическая космическая одиссея о любви, времени и жертве ради будущих поколений."
+  },
+  {
+    id: 5,
+    title: "Безумный Макс: Дорога ярости",
+    genre: "боевик, приключения",
+    image: madMax,
+    imagePath: 'assets/images/MadMaxFuryRoad.png',
+    shortDesc: "Макс помогает беглой Императрице спастись от тирана в пустошах.",
+    year: 2015,
+    director: "Джордж Миллер",
+    fullDesc: "Адреналиновое приключение по постапокалиптической пустыне с погонями и бунтом."
+  },
+  {
+    id: 6,
+    title: "Бесславные ублюдки",
+    genre: "военный, боевик, драма",
+    image: bastards,
+    imagePath: 'assets/images/InglouriousBasterds.png',
+    shortDesc: "Отряд американских евреев мстит нацистам, сея ужас в рядах Третьего рейха.",
+    year: 2009,
+    director: "Квентин Тарантино",
+    fullDesc: "Лейтенант Альдо Рейн и его 'ублюдки' устраивают кровавую бойню, чтобы уничтожить нацистскую верхушку."
+  },
+  {
+    id: 7,
+    title: "Омерзительная восьмёрка",
+    genre: "вестерн, триллер",
+    image: hatefulEight,
+    imagePath: 'assets/images/HatefulEight.png',
+    shortDesc: "В снежной Вайоминге охотник за головами и его заключённая сталкиваются с незнакомцами.",
+    year: 2015,
+    director: "Квентин Тарантино",
+    fullDesc: "История о недоверии, насилии и выживании в замкнутом пространстве после Гражданской войны."
+  },
+  {
+    id: 8,
+    title: "Джокер",
+    genre: "драма, триллер",
+    image: joker,
+    imagePath: 'assets/images/Joker2019.png',
+    shortDesc: "История превращения Артура Флека в Джокера — символа анархии.",
+    year: 2019,
+    director: "Тодд Филлипс",
+    fullDesc: "Темный и психологически насыщенный портрет одиночества и безумия в обществе без сочувствия."
+  }
+]
